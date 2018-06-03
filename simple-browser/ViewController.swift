623fcb30dfb0cd6da71aebeb8fb8fdf09600739f
Liu Teng Hou <liutenghou@gmail.com>
2018-06-03 09:45:16 -0700

    first commit

---
+++ simple-browser/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  simple-browser
+//
+//  Created by Liu Teng Hou on 6/2/18.
+//  Copyright © 2018 wewantcookies. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
